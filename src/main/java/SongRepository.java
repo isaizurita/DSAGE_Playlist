ed0558661344b3e273db6ee629f0431a2d14863a
isaizurita <141947909+isaizurita@users.noreply.github.com>
2025-01-18 01:10:34 -0600

    Update SongRepository.java

--- src/main/java/SongRepository.java
+++ src/main/java/SongRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SongRepository extends JpaRepository<Song, Integer> {
-    // Define methods as needed
-}
+public interface SongRepository extends JpaRepository<Song, Integer> {}
